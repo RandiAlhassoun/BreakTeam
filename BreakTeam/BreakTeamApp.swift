4a016acb7ff3e26da00961dca79a893472dff3a4
RAND ALHASSOUN <rialhassoun22@twq.idserve.net>
2022-12-25 09:38:50 +0300

    Initial Commit

---
+++ BreakTeam/BreakTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakTeamApp.swift
+//  BreakTeam
+//
+//  Created by Rand Alhassoun on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
